107b1daa27dc39b7c79fe24711e2cf15d44e9596
Dan Gulkin <111271073+dangulkin@users.noreply.github.com>
2023-10-06 22:28:52 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-// const express = require('express');
-// const path = require('path');
-// const app = express();
-// const PORT = 3000;
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 3000;
 
-// app.use(express.static('dist'));
-// app.use(express.static('static'));
+app.use(express.static('dist'));
+app.use(express.static('static'));
 
-// app.use('/assets/static', express.static(__dirname + '/static'));
+app.use('/assets/static', express.static(__dirname + '/static'));
 
-// app.use('/*', (req, res) => {
-//   res.sendFile(__dirname + '/dist/index.html');
-// });
+app.use('/*', (req, res) => {
+  res.sendFile(__dirname + '/dist/index.html');
+});
 
 
-// // Start the Express server
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
+// Start the Express server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
