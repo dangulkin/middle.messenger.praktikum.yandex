ecc144d7fc5baa5328e321a63d6bd70db68e8227
Dan Gulkin <d.gulyaikin@gmail.com>
2023-10-06 21:13:23 +0100

    Настроил работу приложения на 3000 порту через express.js

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('dist'));
+app.use(express.static('static'));
+
+app.use('/assets/static', express.static(__dirname + '/static'));
+
+app.use('/*', (req, res) => {
+  res.sendFile(__dirname + '/dist/index.html');
+});
+
+
+// Start the Express server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
